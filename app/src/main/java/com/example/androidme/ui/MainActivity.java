2940b45c60dc4dc6b6338adda256d9939acafcc3
ahmed.elmohame1997.ae@gmail.com <ahmed.elmohame1997.ae@gmail.com>
2020-09-16 14:13:02 +0200

    added TwoPaneUI

--- app/src/main/java/com/example/androidme/ui/MainActivity.java
+++ app/src/main/java/com/example/androidme/ui/MainActivity.java
@@ -27,6 +27,40 @@ public class MainActivity extends AppCompatActivity implements MasterListFragmen
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if (findViewById(R.id.android_me_linear_layout) != null) {
+            mTwoPane = true;
+
+            Button nextButton = findViewById(R.id.next_button);
+            nextButton.setVisibility(View.GONE);
+
+            GridView gridView = findViewById(R.id.images_grid_view);
+            gridView.setNumColumns(2);
+
+            if (savedInstanceState == null) {
+                FragmentManager fragmentManager = getSupportFragmentManager();
+
+                BodyPartFragment headFragment = new BodyPartFragment();
+                headFragment.setmImageIds(AndroidImageAssets.getHeads());
+                fragmentManager.beginTransaction()
+                        .add(R.id.head_container, headFragment)
+                        .commit();
+
+                BodyPartFragment bodyFragment = new BodyPartFragment();
+                bodyFragment.setmImageIds(AndroidImageAssets.getBodies());
+                fragmentManager.beginTransaction()
+                        .add(R.id.body_container, bodyFragment)
+                        .commit();
+
+                BodyPartFragment legsFragment = new BodyPartFragment();
+                legsFragment.setmImageIds(AndroidImageAssets.getLegs());
+                fragmentManager.beginTransaction()
+                        .add(R.id.legs_container, legsFragment)
+                        .commit();
+            }
+        } else {
+            mTwoPane = false;
+        }
     }
 
     @Override
@@ -37,19 +71,53 @@ public class MainActivity extends AppCompatActivity implements MasterListFragmen
 
         int listIndex = position - 12 * bodyPartNumber;
 
-        switch (bodyPartNumber) {
-            case 0:
-                headIndex = listIndex;
-                break;
-            case 1:
-                bodyIndex = listIndex;
-                break;
-            case 2:
-                legIndex = listIndex;
-                break;
+        if (mTwoPane) {
+            BodyPartFragment bodyPartFragment = new BodyPartFragment();
+
+            switch (bodyPartNumber) {
+                case 0:
+                    bodyPartFragment.setmImageIds(AndroidImageAssets.getHeads());
+                    bodyPartFragment.setmListIndex(listIndex);
+                    getSupportFragmentManager()
+                            .beginTransaction()
+                            .replace(R.id.head_container, bodyPartFragment)
+                            .commit();
+                    break;
+                case 1:
+                    bodyPartFragment.setmImageIds(AndroidImageAssets.getBodies());
+                    bodyPartFragment.setmListIndex(listIndex);
+                    getSupportFragmentManager()
+                            .beginTransaction()
+                            .replace(R.id.body_container, bodyPartFragment)
+                            .commit();
+                    break;
+                case 2:
+                    bodyPartFragment.setmImageIds(AndroidImageAssets.getLegs());
+                    bodyPartFragment.setmListIndex(listIndex);
+                    getSupportFragmentManager()
+                            .beginTransaction()
+                            .replace(R.id.legs_container, bodyPartFragment)
+                            .commit();
+                    break;
+                default:
+                    break;
+            }
+        } else {
+
+            switch (bodyPartNumber) {
+                case 0:
+                    headIndex = listIndex;
+                    break;
+                case 1:
+                    bodyIndex = listIndex;
+                    break;
+                case 2:
+                    legIndex = listIndex;
+                    break;
+            }
         }
 
-        // Put this information in a Bundle and attach it to an Intent that will launch an AndroidMeActivity
+        // Put this information in a Bundle and attach it to an Intent that will launch AndroidMeActivity
         Bundle b = new Bundle();
         b.putInt("headIndex", headIndex);
         b.putInt("bodyIndex", bodyIndex);
